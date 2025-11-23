8b876cf64167f9c941f112f235c0fee34c54a745
skripka-max <zmurovarik107@gmail.com>
2025-11-23 15:23:22 +0300

    added base class of platform

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ window = display.set_mode((900, 800))
 display.set_caption('ping-pong')
 timer = time.Clock()
 game = True
+
+class Platform(sprite.Sprite):
+    def __init__(self, file_name, x, y):
+        self.image = image.load(file_name)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 while game:
     window.fill((200, 200, 255))
     for e in event.get():
