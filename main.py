a3d2587c04ed9533bb9f53970f119dbb758192d7
skripka-max <zmurovarik107@gmail.com>
2025-11-23 15:32:41 +0300

    reworked game sprite for the ball obj

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ display.set_caption('ping-pong')
 timer = time.Clock()
 game = True
 
-class Platform(sprite.Sprite):
-    def __init__(self, file_name, x, y):
-        self.image = image.load(file_name)
+class GameSprite(sprite.Sprite):
+    def __init__(self, file_name, x, y, width = 30, height = 200):
+        self.image = transform.scale(image.load(file_name), (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-player_left = Platform('player-left.png', 5, 300)
-player_right = Platform('player-right.png', 865, 300)
+player_left = GameSprite('player-left.png', 5, 300)
+player_right = GameSprite('player-right.png', 865, 300)
+ball = GameSprite('ball.png', 400, 350, width = 100, height = 100)
 
 while game:
     window.fill((200, 200, 255))
@@ -23,5 +24,6 @@ while game:
             game = False
     player_left.draw()
     player_right.draw()
+    ball.draw()
     display.update()
     timer.tick(60)
